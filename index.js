bea96fa41b7abbf74156f9bcbb2c297175163ccd
Damian Bushong <katana@odios.us>
2015-08-08 10:14:52 -0500

    working on display code and formatting

--- index.js
+++ index.js
@@ -10,7 +10,8 @@
 //
 'use strict'
 
-let path = require('path')
+let path = require('path'),
+	util = require('util')
 
 let jade = require('jade'),
 	nl2br = require('nl2br'),
@@ -85,38 +86,52 @@ module.exports = function enclosure(title) {
 					},
 					frames: stackTrace.parse(exception).map(frameModifier)
 					// note: this.req is the underlying request object - it appears to bypass koa
-					serverInfo: {
-						REMOTE_ADDR: this.request.ip,
-            //SERVER_SOFTWARE: 'NodeJS ' + process.versions.node + " " + os.type(),
-            SERVER_PROTOCOL: this.request.protocol() + "/" + this.req.httpVersion,
-            SCRIPT_FILE: require.main.filename,
-            REQUEST_URI: this.request.url,
-            REQUEST_METHOD: this.request.method,
-            PATH_INFO: this.require.path,
-            QUERY_STRING: this.request.querystring,
-            HTTP_HOST: this.request.host,
-            HTTP_CONNECTION: this.request.get('connection'),
-            HTTP_CACHE_CONTROL: this.request.get('cache-control'),
-            HTTP_ACCEPT: this.request.get('accept'),
-            HTTP_USER_AGENT: this.request.get('user-agent']),
-            HTTP_DNT: this.request.get('dnt'),
-            HTTP_ACCEPT_ENCODING: this.request.get('accept-encoding'),
-            HTTP_ACCEPT_LANGUAGE: this.request.get('accept-language'),
-            HTTP_COOKIE: this.request.get('cookie')
+					tables: {
+						'Server/Request Data': {
+							REMOTE_ADDR: this.request.ip,
+	            //SERVER_SOFTWARE: 'NodeJS ' + process.versions.node + " " + os.type(),
+	            SERVER_PROTOCOL: this.request.protocol() + "/" + this.req.httpVersion,
+	            SCRIPT_FILE: require.main.filename,
+	            REQUEST_URI: this.request.url,
+	            REQUEST_METHOD: this.request.method,
+	            PATH_INFO: this.require.path,
+	            QUERY_STRING: this.request.querystring,
+	            HTTP_HOST: this.request.host,
+	            HTTP_CONNECTION: this.request.get('connection'),
+	            HTTP_CACHE_CONTROL: this.request.get('cache-control'),
+	            HTTP_ACCEPT: this.request.get('accept'),
+	            HTTP_USER_AGENT: this.request.get('user-agent']),
+	            HTTP_DNT: this.request.get('dnt'),
+	            HTTP_ACCEPT_ENCODING: this.request.get('accept-encoding'),
+	            HTTP_ACCEPT_LANGUAGE: this.request.get('accept-language'),
+	            HTTP_COOKIE: this.request.get('cookie')
+						},
+						'Request Parameters': {
+							queryString: this.request.query,
+							requestBody: this.req.body,
+
+						},
+						'Cookies': {},
+						'Environment variables': process.env
+					}
+					asset: {
+						script: {
+							zepto: fs.readFileSync(path.join(__dirname, 'assets/js', 'zepto.min.js')),
+							prettify: fs.readFileSync(path.join(__dirname, 'assets/js', 'prettify.js'))
+							winceJS: fs.readFileSync(path.join(__dirname, 'assets/js', 'wince.js'))
+						},
+						style: {
+							winceCSS: fs.readFileSync(path.join(__dirname, 'assets/css', 'wince.css'))
+						}
 					},
-					requestParams: {
-						queryString: this.request.query,
-						requestBody: this.req.body,
-						cookies: {}
-					},
-					envVars: process.env
+					util: util
 				}
 
 				// quick parsing of cookies for inspection
 				let cookie = this.request.get('cookie')
 				cookie.split(';').forEach(function (cookie) {
           var parts = cookie.split('=');
-          tplCtx.requestParams.cookies[parts.shift().trim()] = decodeURI(parts.join('='));
+          tplCtx.tables['Cookies'][parts.shift().trim()] = decodeURI(parts.join('='));
         })
 
 				if(!jadeFn) {
@@ -129,6 +144,8 @@ module.exports = function enclosure(title) {
 
 				this.body = jadeFn(tplCtx)
 				this.status = err.status || 500
+
+				this.trace('wince-done')
 			}
 		}
 	}
