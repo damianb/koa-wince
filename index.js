adf08168fa422de1f6ce50a92701ea04353c34db
Damian Bushong <katana@odios.us>
2015-08-08 10:17:11 -0500

    fix indentation

--- index.js
+++ index.js
@@ -41,19 +41,19 @@ module.exports = function enclosure(title) {
 				}
 
 				// Return null if the file doesn't actually exist.
-        if (!fs.existsSync(filePath)) {
-          if (process.env.NVM_DIR && process.versions.node) {
-            filePath = path.join(process.env.NVM_DIR, 'src/node-v' + process.versions.node, 'lib', filePath);
-            if (!fs.existsSync(filePath)) {
-                return null;
-            }
-          } else {
-            return null;
-          }
-        }
+				if (!fs.existsSync(filePath)) {
+					if (process.env.NVM_DIR && process.versions.node) {
+						filePath = path.join(process.env.NVM_DIR, 'src/node-v' + process.versions.node, 'lib', filePath);
+						if (!fs.existsSync(filePath)) {
+								return null;
+						}
+					} else {
+						return null;
+					}
+				}
 
-        contents = fs.readFileSync(filePath, 'utf-8')
-        return contents.split("\n").slice(start, start + length)
+				contents = fs.readFileSync(filePath, 'utf-8')
+				return contents.split("\n").slice(start, start + length)
 			}
 		})
 	}
@@ -89,22 +89,22 @@ module.exports = function enclosure(title) {
 					tables: {
 						'Server/Request Data': {
 							REMOTE_ADDR: this.request.ip,
-	            //SERVER_SOFTWARE: 'NodeJS ' + process.versions.node + " " + os.type(),
-	            SERVER_PROTOCOL: this.request.protocol() + "/" + this.req.httpVersion,
-	            SCRIPT_FILE: require.main.filename,
-	            REQUEST_URI: this.request.url,
-	            REQUEST_METHOD: this.request.method,
-	            PATH_INFO: this.require.path,
-	            QUERY_STRING: this.request.querystring,
-	            HTTP_HOST: this.request.host,
-	            HTTP_CONNECTION: this.request.get('connection'),
-	            HTTP_CACHE_CONTROL: this.request.get('cache-control'),
-	            HTTP_ACCEPT: this.request.get('accept'),
-	            HTTP_USER_AGENT: this.request.get('user-agent']),
-	            HTTP_DNT: this.request.get('dnt'),
-	            HTTP_ACCEPT_ENCODING: this.request.get('accept-encoding'),
-	            HTTP_ACCEPT_LANGUAGE: this.request.get('accept-language'),
-	            HTTP_COOKIE: this.request.get('cookie')
+							//SERVER_SOFTWARE: 'NodeJS ' + process.versions.node + " " + os.type(),
+							SERVER_PROTOCOL: this.request.protocol() + "/" + this.req.httpVersion,
+							SCRIPT_FILE: require.main.filename,
+							REQUEST_URI: this.request.url,
+							REQUEST_METHOD: this.request.method,
+							PATH_INFO: this.require.path,
+							QUERY_STRING: this.request.querystring,
+							HTTP_HOST: this.request.host,
+							HTTP_CONNECTION: this.request.get('connection'),
+							HTTP_CACHE_CONTROL: this.request.get('cache-control'),
+							HTTP_ACCEPT: this.request.get('accept'),
+							HTTP_USER_AGENT: this.request.get('user-agent']),
+							HTTP_DNT: this.request.get('dnt'),
+							HTTP_ACCEPT_ENCODING: this.request.get('accept-encoding'),
+							HTTP_ACCEPT_LANGUAGE: this.request.get('accept-language'),
+							HTTP_COOKIE: this.request.get('cookie')
 						},
 						'Request Parameters': {
 							queryString: this.request.query,
@@ -130,9 +130,9 @@ module.exports = function enclosure(title) {
 				// quick parsing of cookies for inspection
 				let cookie = this.request.get('cookie')
 				cookie.split(';').forEach(function (cookie) {
-          var parts = cookie.split('=');
-          tplCtx.tables['Cookies'][parts.shift().trim()] = decodeURI(parts.join('='));
-        })
+					var parts = cookie.split('=');
+					tplCtx.tables['Cookies'][parts.shift().trim()] = decodeURI(parts.join('='));
+				})
 
 				if(!jadeFn) {
 					let jadeFilename = path.join(__dirname, 'assets/views', 'wince.body.jade')
